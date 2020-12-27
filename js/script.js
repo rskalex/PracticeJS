ebc1cd9e6fe1bd2d41f9ed706f0319979a75c3c8
Alexander <dobrovolya48@gmail.com>
2020-12-27 22:05:48 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// let ranbomBodyParts = ['Глаза', 'Волосы', 'Бедра'];
+// let randomAdjective = ['Прекрасны', 'Восхитительны', 'Красивы'];
+// let randomWords = ['Небо', 'Звезды', 'Море', 'Снег', 'Огонь'];
+
+// ranbomBodyParts = ranbomBodyParts[Math.floor(Math.random() *3)];
+// randomAdjective = randomAdjective[Math.floor(Math.random() * (randomAdjective.length))];
+// randomWords = randomWords[Math.floor(Math.random() * randomWords.length)];
+
+// let randomСompliment = 'У тебя ' + ranbomBodyParts + ' ' + randomAdjective + ' ' + 'как' + ' ' + randomWords + '!!!';
+
+// let randomСomplimentTwo = ['У тебя', ranbomBodyParts, randomAdjective, 'как', randomWords, '!!!'].join(' ');
+
+// console.log(randomСompliment);
+// console.log(randomСomplimentTwo);
+
+// let arr = [3, 2, 1];
+// let newArr= '';
+
+// for ( let i = 0; i < arr.length; i++){
+//     newArr = newArr + arr[i] + ' больше, чем ';
+
+// }
+
+// console.log(newArr.slice(0, 29));
+
+
+let i = 41;
+
+console.log(i++);
+console.log(i);
+console.log(i--);
+console.log(i);
