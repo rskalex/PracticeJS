7f9f7fccb25f298ceb4e4fd0e93a8a79348503be
Alexander <dobrovolya48@gmail.com>
2020-12-27 22:19:50 +0300

    modified script.js file

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
-// let ranbomBodyParts = ['Глаза', 'Волосы', 'Бедра'];
-// let randomAdjective = ['Прекрасны', 'Восхитительны', 'Красивы'];
-// let randomWords = ['Небо', 'Звезды', 'Море', 'Снег', 'Огонь'];
+let ranbomBodyParts = ['Глаза', 'Волосы', 'Бедра'];
+let randomAdjective = ['Прекрасны', 'Восхитительны', 'Красивы'];
+let randomWords = ['Небо', 'Звезды', 'Море', 'Снег', 'Огонь'];
 
-// ranbomBodyParts = ranbomBodyParts[Math.floor(Math.random() *3)];
-// randomAdjective = randomAdjective[Math.floor(Math.random() * (randomAdjective.length))];
-// randomWords = randomWords[Math.floor(Math.random() * randomWords.length)];
+ranbomBodyParts = ranbomBodyParts[Math.floor(Math.random() *3)];
+randomAdjective = randomAdjective[Math.floor(Math.random() * (randomAdjective.length))];
+randomWords = randomWords[Math.floor(Math.random() * randomWords.length)];
 
-// let randomСompliment = 'У тебя ' + ranbomBodyParts + ' ' + randomAdjective + ' ' + 'как' + ' ' + randomWords + '!!!';
+let randomСompliment = 'У тебя ' + ranbomBodyParts + ' ' + randomAdjective + ' ' + 'как' + ' ' + randomWords + '!!!';
 
-// let randomСomplimentTwo = ['У тебя', ranbomBodyParts, randomAdjective, 'как', randomWords, '!!!'].join(' ');
+let randomСomplimentTwo = ['У тебя', ranbomBodyParts, randomAdjective, 'как', randomWords, '!!!'].join(' ');
 
-// console.log(randomСompliment);
-// console.log(randomСomplimentTwo);
+console.log(randomСompliment);
+console.log(randomСomplimentTwo);
 
-// let arr = [3, 2, 1];
-// let newArr= '';
+let arr = [3, 2, 1];
+let newArr= '';
 
-// for ( let i = 0; i < arr.length; i++){
-//     newArr = newArr + arr[i] + ' больше, чем ';
+for ( let i = 0; i < arr.length; i++){
+    newArr = newArr + arr[i] + ' больше, чем ';
 
-// }
+}
 
-// console.log(newArr.slice(0, 29));
+console.log(newArr.slice(0, 29));
 
+
+// 
 
 let i = 41;
 
